bf70a47d8d16e226645953cc7417dbaf149939e4
elune569 <elune569@naver.com>
2021-03-19 16:51:16 +0900

    210319

--- j_10951.java
+++ j_10951.java
@@ -1,20 +1,21 @@
 package Training;
 import java.util.*;
+/*
+  이클립스랑 인텔리제이에서는 보이는 문제는 없이 잘 돌아간다.
+ */
 import java.io.*;
 public class j_10951 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-
-		boolean run = true;
+		String input = null;
 		
-		while(run) {
-			st = new StringTokenizer(br.readLine());
-			int ct = st.countTokens();
-			while(st.hasMoreTokens())
-				bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
-			if(ct == 0) break;
+		
+		while((input = br.readLine()) != null && input.length() != 0) {
+			StringTokenizer st = new StringTokenizer(input);
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			bw.write(String.valueOf(num1 + num2)+"\n");
 		}
 		bw.close();
 	}
